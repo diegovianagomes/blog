b8918c44741848d593fed6182d98e2fff0ab11b5
dvgba <diegovianagomes01@gmail.com>
2025-07-08 12:44:00 -0300

    refactor: reorganize imports and improve code structure in ArticlePage

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,3 +1,5 @@
+
+import { notFound } from 'next/navigation'
 import { Article } from "@/components/article";
 import { getArticlesData } from "@/lib/articles";
 
@@ -11,28 +13,35 @@ function Tag({ tag }: { tag: string }) {
   );
 }
 
-export default async function ArticlePage({
-  params,
-}: {
-  params: { slug: string }
-}) {
-  const articleData = await getArticlesData(params.slug);
+type Props = {
+  params: {
+    slug: string
+  }
+}
+
+export default async function ArticlePage({ params }: Props) {
+  const articleData = await getArticlesData(params.slug)
+
+  if (!articleData) {
+    notFound()
+  }
+
   return (
     <>
       <div className="flex flex-col items-center justify-center p-8">
-          <span className="text-sm text-zinc-500 dark:text-zinc-400">
-            {articleData.date}
-          </span>
-          <div className="flex w-full max-w-prose flex-col items-center justify-center gap-4 px-4 pb-6 pt-1">
-            <h1 className="text-center max-sm:text-3xl text-5xl">
-              {articleData.title}
-            </h1>
-            <div className="flex flex-wrap gap-x-3">
-              {articleData.tags.map((tag: string) => (
-                <Tag key={tag} tag={tag} />
-              ))}
-            </div>
+        <span className="text-sm text-zinc-500 dark:text-zinc-400">
+          {articleData.date}
+        </span>
+        <div className="flex w-full max-w-prose flex-col items-center justify-center gap-4 px-4 pb-6 pt-1">
+          <h1 className="text-center text-5xl max-sm:text-3xl">
+            {articleData.title}
+          </h1>
+          <div className="flex flex-wrap justify-center gap-x-3">
+            {articleData.tags.map((tag: string) => (
+              <Tag key={tag} tag={tag} />
+            ))}
           </div>
+        </div>
       </div>
       <Article html={articleData.contentHtml} />
     </>
