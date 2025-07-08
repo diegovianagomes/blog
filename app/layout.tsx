826e42ba37f8980d0e794e9b5145705966e4a2a9
dvgba <diegovianagomes01@gmail.com>
2025-07-08 11:57:23 -0300

    feat: comment out unused Script import in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./Header";
-import Script from "next/script";
+//import Script from "next/script";
 
 
 const geistSans = Geist({
